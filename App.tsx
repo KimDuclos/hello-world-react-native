faa93911a06df26f003102719aec040b87333fc8
Kim Duclos <kimduclos@gmail.com>
2023-01-13 14:01:04 -0800

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>SeanDon is a poopy</Text>
       <StatusBar style="auto" />
     </View>
   );
